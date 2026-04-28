c6d5ce4d85482a6bbae472db39765e53a0dc37e6
quanennathaniel <91539686+quanennathaniel@users.noreply.github.com>
2026-04-28 20:20:56 +0800

    Update esppac.h

--- components/panasonic_ac/esppac.h
+++ components/panasonic_ac/esppac.h
@@ -13,21 +13,20 @@ namespace panasonic_ac {
 
 static const char *const VERSION = "2.5.0";
 
-static const uint8_t BUFFER_SIZE = 128;  // The maximum size of a single packet (both receive and transmit)
-static const uint8_t READ_TIMEOUT = 20;  // The maximum time to wait before considering a packet complete
+static const uint8_t BUFFER_SIZE = 128;
+static const uint8_t READ_TIMEOUT = 20;
 
-static const uint8_t MIN_TEMPERATURE = 16;     // Minimum temperature as reported by Panasonic app
-static const uint8_t MAX_TEMPERATURE = 30;     // Maximum temperature as supported by Panasonic app
-static const float TEMPERATURE_STEP = 0.5;     // Steps the temperature can be set in
-static const float TEMPERATURE_TOLERANCE = 2;  // The tolerance to allow when checking the climate state
-static const uint8_t TEMPERATURE_THRESHOLD =
-    100;  // Maximum temperature the AC can report before considering the temperature as invalid
+static const uint8_t MIN_TEMPERATURE = 16;
+static const uint8_t MAX_TEMPERATURE = 30;
+static const float TEMPERATURE_STEP = 0.5;
+static const float TEMPERATURE_TOLERANCE = 2;
+static const uint8_t TEMPERATURE_THRESHOLD = 100;
 
 enum class CommandType { Normal, Response, Resend };
 
 enum class ACType {
-  DNSKP11,  // New module (via CN-WLAN)
-  CZTACG1   // Old module (via CN-CNT)
+  DNSKP11,
+  CZTACG1
 };
 
 class PanasonicAC : public Component, public uart::UARTDevice, public climate::Climate {
@@ -37,6 +36,7 @@ class PanasonicAC : public Component, public uart::UARTDevice, public climate::C
   void set_vertical_swing_select(select::Select *vertical_swing_select);
   void set_horizontal_swing_select(select::Select *horizontal_swing_select);
   void set_nanoex_switch(switch_::Switch *nanoex_switch);
+  void set_nanoeg_switch(switch_::Switch *nanoeg_switch);
   void set_eco_switch(switch_::Switch *eco_switch);
   void set_econavi_switch(switch_::Switch *econavi_switch);
   void set_mild_dry_switch(switch_::Switch *mild_dry_switch);
@@ -49,37 +49,36 @@ class PanasonicAC : public Component, public uart::UARTDevice, public climate::C
   void loop() override;
 
  protected:
-  sensor::Sensor *outside_temperature_sensor_ = nullptr;        // Sensor to store outside temperature from queries
-  select::Select *vertical_swing_select_ = nullptr;             // Select to store manual position of vertical swing
-  select::Select *horizontal_swing_select_ = nullptr;           // Select to store manual position of horizontal swing
+  sensor::Sensor *outside_temperature_sensor_ = nullptr;
+  select::Select *vertical_swing_select_ = nullptr;
+  select::Select *horizontal_swing_select_ = nullptr;
   switch_::Switch *nanoex_switch_ = nullptr;                    // Switch to toggle nanoeX on/off
-  switch_::Switch *eco_switch_ = nullptr;                       // Switch to toggle eco mode on/off
-  switch_::Switch *econavi_switch_ = nullptr;                   // Switch to toggle econavi mode on/off
-  switch_::Switch *mild_dry_switch_ = nullptr;                  // Switch to toggle mild dry mode on/off
-  sensor::Sensor *current_temperature_sensor_ = nullptr;        // Sensor to use for current temperature where AC does not report
-  sensor::Sensor *current_power_consumption_sensor_ = nullptr;  // Sensor to store current power consumption from queries
+  switch_::Switch *nanoeg_switch_ = nullptr;                    // Switch to toggle nanoe-G on/off
+  switch_::Switch *eco_switch_ = nullptr;
+  switch_::Switch *econavi_switch_ = nullptr;
+  switch_::Switch *mild_dry_switch_ = nullptr;
+  sensor::Sensor *current_temperature_sensor_ = nullptr;
+  sensor::Sensor *current_power_consumption_sensor_ = nullptr;
 
   size_t vertical_swing_state_;
   size_t horizontal_swing_state_;
 
-  int8_t current_temperature_offset_ = 0;  // current temperature offset to compensate internal sensor values
-  int8_t outside_temperature_offset_ = 0;  // outside temperature offset to compensate internal sensor values
-  bool nanoex_state_ = false;    // Stores the state of nanoex to prevent duplicate packets
-  bool eco_state_ = false;       // Stores the state of eco to prevent duplicate packets
-  bool econavi_state_ = false;       // Stores the state of econavi to prevent duplicate packets
-  bool mild_dry_state_ = false;  // Stores the state of mild dry to prevent duplicate packets
+  int8_t current_temperature_offset_ = 0;
+  int8_t outside_temperature_offset_ = 0;
+  bool nanoex_state_ = false;
+  bool nanoeg_state_ = false;                                   // Stores the state of nanoe-G to prevent duplicate packets
+  bool eco_state_ = false;
+  bool econavi_state_ = false;
+  bool mild_dry_state_ = false;
 
-  bool waiting_for_response_ = false;  // Set to true if we are waiting for a response
-
-  // uint8_t receive_buffer_index = 0;     // Current position of the receive buffer
-  // uint8_t receive_buffer[BUFFER_SIZE];  // Stores the packet currently being received
+  bool waiting_for_response_ = false;
 
   std::vector<uint8_t> rx_buffer_;
 
-  uint32_t init_time_;             // Stores the current time
-  uint32_t last_read_;             // Stores the time at which the last read was done
-  uint32_t last_packet_sent_;      // Stores the time at which the last packet was sent
-  uint32_t last_packet_received_;  // Stores the time at which the last packet was received
+  uint32_t init_time_;
+  uint32_t last_read_;
+  uint32_t last_packet_sent_;
+  uint32_t last_packet_received_;
 
   climate::ClimateTraits traits() override;
 
@@ -91,6 +90,7 @@ class PanasonicAC : public Component, public uart::UARTDevice, public climate::C
   void update_swing_horizontal(const StringRef &swing);
   void update_swing_vertical(const StringRef &swing);
   void update_nanoex(bool nanoex);
+  void update_nanoeg(bool nanoeg);
   void update_eco(bool eco);
   void update_econavi(bool econavi);
   void update_mild_dry(bool mild_dry);
@@ -99,6 +99,7 @@ class PanasonicAC : public Component, public uart::UARTDevice, public climate::C
   virtual void on_horizontal_swing_change(const StringRef &swing) = 0;
   virtual void on_vertical_swing_change(const StringRef &swing) = 0;
   virtual void on_nanoex_change(bool nanoex) = 0;
+  virtual void on_nanoeg_change(bool nanoeg) = 0;
   virtual void on_eco_change(bool eco) = 0;
   virtual void on_econavi_change(bool econavi) = 0;
   virtual void on_mild_dry_change(bool mild_dry) = 0;
